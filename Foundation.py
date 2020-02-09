5ce486ec5db33416b116f5b96c0f5d6a181240b6
brucenielson <brucenielson@users.noreply.github.com>
2020-02-09 09:02:18 -0700

    Foundation Predictions

--- Foundation.py
+++ Foundation.py
@@ -22,11 +22,19 @@ def create_train_test():
     datafile = os.getcwd() + '\\DonationData.csv'
     donation_data = pd.read_csv(datafile, dtype={'Alias':int})
     donation_data['Donated'] = (donation_data['FirstDonationAmount'] > 0.0)
-    donation_data = drop_columns(donation_data, ['M6Date','FirstDate','FirstState','M6State','Alias','M6Rank','M6TeneureAtRank','M6TenureMonths','PersonalEnrollments_M6Date','FirstDonationAmount'])
-    donation_data = rename_column(donation_data, 'FirstDRank', 'Rank')
-    donation_data = rename_column(donation_data, 'FirstTenureAtRank', 'TenureAtRank')
-    donation_data = rename_column(donation_data, 'FirstTenureMonths', 'TenureAtMonths')
-    donation_data = rename_column(donation_data, 'PersonalEnrollments_FirstDate', 'PersonalEnrollments')
+
+    # donation_data = drop_columns(donation_data, ['M6Date','FirstDate','FirstState','M6State','Alias','M6Rank','M6TeneureAtRank','M6TenureMonths','PersonalEnrollments_M6Date','FirstDonationAmount'])
+    # donation_data = rename_column(donation_data, 'FirstDRank', 'Rank')
+    # donation_data = rename_column(donation_data, 'FirstTenureAtRank', 'TenureAtRank')
+    # donation_data = rename_column(donation_data, 'FirstTenureMonths', 'TenureAtMonths')
+    # donation_data = rename_column(donation_data, 'PersonalEnrollments_FirstDate', 'PersonalEnrollments')
+
+    donation_data = drop_columns(donation_data, ['M6Date','FirstDate','FirstState','M6State','Alias','FirstDRank','FirstTenureAtRank','FirstTenureMonths','PersonalEnrollments_FirstDate','FirstDonationAmount'])
+    donation_data = rename_column(donation_data, 'M6Rank', 'Rank')
+    donation_data = rename_column(donation_data, 'M6TeneureAtRank', 'TenureAtRank')
+    donation_data = rename_column(donation_data, 'M6TenureMonths', 'TenureAtMonths')
+    donation_data = rename_column(donation_data, 'PersonalEnrollments_M6Date', 'PersonalEnrollments')
+
 
     # Get control data
     datafile = os.getcwd() + '\\ControlData.csv'
@@ -77,6 +85,11 @@ def print_correlations(X, y):
     print(correlations)
 
 
+def set_prediction(data, probability):
+    data['Prediction'] = data['ProbTrue'] >= probability
+    return data
+
+
 def stats(clf, X, y):
     # Predict
     y_pred = clf.predict(X)
@@ -99,6 +112,11 @@ def stats(clf, X, y):
     all_data = pd.concat([X, y], axis=1)
     all_data.reset_index(drop=True, inplace=True)
     all_data = pd.concat([all_data, y_pred], axis=1)
+    y_pred_prob = clf.predict_proba(X)
+    y_pred_prob = pd.DataFrame(y_pred_prob, columns=['ProbFalse', "ProbTrue"])
+    all_data = pd.concat([all_data, y_pred_prob['ProbTrue']], axis=1)
+    # print(all_data.head(5))
+
 
     # How accurate are the positive predictions?
     true_predictions = all_data[all_data['Prediction'] == True]
@@ -107,21 +125,18 @@ def stats(clf, X, y):
     X_postitive = true_predictions.drop(['Prediction'], axis=1)
     X_postitive = X_postitive.drop(['Donated'], axis=1)
 
-    print_stats("Positive Predictions", X_postitive, y_positive, y_positive_pred)
+    print_stats("Positive Predictions 50%", X_postitive, y_positive, y_positive_pred)
 
+    all_data = set_prediction(all_data, 0.10)
+    # How accurate are the positive predictions?
+    true_predictions = all_data[all_data['Prediction'] == True]
+    y_positive_pred = true_predictions['Prediction']
+    y_positive = true_predictions['Donated']
+    X_postitive = true_predictions.drop(['Prediction'], axis=1)
+    X_postitive = X_postitive.drop(['Donated'], axis=1)
 
+    print_stats("Positive Predictions 10%", X_postitive, y_positive, y_positive_pred)
 
-
-
-def test(clf, X, y):
-    # returns statistics
-    y_pred = clf.predict(X)
-    y_pred = y_pred[0:len(X)]
-    print("")
-    print('Total Samples:', len(X))
-    print("Results of Predict - Test Set:")
-    print('Misclassified Test samples: %d' % (y != y_pred).sum())
-    print('Accuracy of Test set: %.4f' % accuracy_score(y, y_pred))
 
 
 
@@ -129,4 +144,4 @@ def test(clf, X, y):
 X_train, y_train, X_test, y_test = create_train_test()
 clf = train(X_train, y_train)
 stats(clf, X_train, y_train)
-# test(clf, X_test, y_test)
+stats(clf, X_test, y_test)
